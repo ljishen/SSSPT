0d786b612d30e9b137f8b4751c59bf9bdce84a98
Jianshen Liu <jliu120@ucsc.edu>
2018-11-15 00:23:16 -0800

    Remove the redundant hdparm security-set-pass + security-erase operation because they are too dangerous

--- playbooks/roles/common/files/blkerasediscard.sh
+++ playbooks/roles/common/files/blkerasediscard.sh
@@ -6,12 +6,12 @@ if [ "$#" -ne 1 ]; then
 	cat <<-ENDOFMESSAGE
 Usage: $0 BLOCK_DEVICE
 
-Secure erase and discard all the sectors on the block device.
+Discard all the sectors on the block device.
 
 Note that this script is EXCEPTIONALLY DANGEROUS.
 See the option --trim-sector-ranges of hdparm.
 
-For secure erase operation, see
+For the differences between trim and secure erase operation, see
 https://www.thomas-krenn.com/en/wiki/SSD_Secure_Erase
 https://storage.toshiba.com/docs/services-support-documents/ssd_application_note.pdf
 ENDOFMESSAGE
@@ -28,29 +28,6 @@ if [ ! -b "$device" ]; then
 	echo "$device is not a block device"
     exit 2
 fi
-
-echo "start secure erasing device $device ..."
-
-if ! hdparm -I "$device" | grep "not\\s*frozen" &> /dev/null || \
-   ! hdparm -I "$device" | grep "supported:\\s*enhanced erase" &> /dev/null ; then
-    printf "device %s is frozen or does not support enhanced erase.
-Please check the output of hdparm -I %s\\n" "$device" "$device"
-    exit 3
-fi
-
-hdparm --user-master u --security-set-pass NULL "$device" > /dev/null
-hdparm --user-master u --security-erase NULL "$device" > /dev/null
-
-if hdparm -I "$device" | grep "not\\s*enabled" &> /dev/null ; then
-    echo "successfully secure erased device $device"
-else
-    printf "failed to disable the security feature.
-Please check the output of hdparm -I %s\\n" "$device"
-    exit 3
-fi
-
-
-printf "\\nstart trimming device %s ...\\n" "$device"
 
 total_sectors="$(blockdev --getsz "$device")"
 echo "total 512-byte $total_sectors on device $device"
