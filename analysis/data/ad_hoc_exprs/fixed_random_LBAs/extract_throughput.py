d1813e6867994df432eb60075b05542880331026
Jianshen Liu <jliu120@ucsc.edu>
2018-11-16 00:35:51 +0000

    Add baseline profiles from rockpro64

--- analysis/data/ad_hoc_exprs/fixed_random_LBAs/extract_throughput.py
+++ analysis/data/ad_hoc_exprs/fixed_random_LBAs/extract_throughput.py
@@ -7,20 +7,29 @@ data_in_nextline = False
 
 throughputs = []
 
+rkB_s_idx = -1
+wkB_s_idx = -1
+
 with open(sys.argv[1], 'r') as fp:
     for line in fp:
         if data_in_nextline:
             if line.strip():
                 nums = line.split()
-                throughputs.append(float(nums[5]) + float(nums[6]))
-            else:
+                throughputs.append(
+                    float(nums[rkB_s_idx]) + float(nums[wkB_s_idx]))
+            elif throughputs:
                 throughputs.append(0)
 
             data_in_nextline = False
             continue
 
-        if 'Device:' in line:
+        if 'Device' in line:
             data_in_nextline = True
+
+            if rkB_s_idx < 0 or wkB_s_idx < 0:
+                headers = line.split()
+                rkB_s_idx = headers.index('rkB/s')
+                wkB_s_idx = headers.index('wkB/s')
 
 with open('throughputs.iostat.csv', 'w') as fp:
     for idx, num in enumerate(throughputs):
